8e0e1ee57dec27a1f37bfe4e6407ba8bb081e768
Ariana Cabana <acabana.0904@gmail.com>
2017-04-27 18:27:58 -0500

    agregando menu-opciones

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,11 +2,29 @@ function creandoDiv(getURL) {
   var contenedor = document.getElementById("contenedor");
   var div = document.createElement("div");
   var img = document.createElement("img");
+  var icon = document.createElement("img");
+  var icon2 = document.createElement("img");
 
   img.setAttribute("src", getURL);
+  img.setAttribute("id","image");
+  // flechas opciones
+  icon.setAttribute("src","assets/img/collapse-arrow.png");
+  icon.setAttribute("id","collapse-arrow");
+  icon.setAttribute("class","icon-arrow");
+  icon2.setAttribute("src","assets/img/expand-arrow.png");
+  icon2.setAttribute("id","expand-arrow");
+  icon2.setAttribute("class","icon-arrow");
+
   div.classList.add("contenedor-image");
   div.appendChild(img);
+  div.appendChild(icon);
+  div.appendChild(icon2);
   contenedor.appendChild(div);
+
+  var arriba = document.getElementById("collapse-arrow");
+   arriba.addEventListener("click",function(){
+    icon.style.display = "none";
+   });
 }
 
 var inputURL = document.getElementById("url");
@@ -16,5 +34,7 @@ function getURL(e){
     this.value = "";
   }
 }
+
+
 
 inputURL.onkeydown = getURL;
